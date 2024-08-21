3dafb09d419297f1f76c9e93e141bad45ecdd8a7
Fabio <fabio.waljaard@gmail.com>
2024-08-21 21:22:17 +0200

    Initial IS31FL3731 component implementation

---
+++ esphome/components/is31fl3731/is31fl3731.cpp
@@ -0,0 +1,90 @@
+#include "is31fl3731.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace is31fl3731 {
+
+static const uint8_t ISSI_COMMANDREGISTER = 0xFD;
+static const uint8_t ISSI_REG_PICTUREFRAME = 0x01;
+static const uint8_t ISSI_BANK_FUNCTIONREG = 0x0B;
+
+static const char *const TAG = "is31fl3731.display";
+
+void IS31FL3731Component::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up BME680...");
+}
+
+void IS31FL3731Component::dump_config() {
+  ESP_LOGCONFIG(TAG, "IS31FL3731:");
+  LOG_I2C_DEVICE(this);
+  if (this->is_failed()) {
+    ESP_LOGE(TAG, "Communication with IS31FL3731 failed!");
+  }
+}
+
+float IS31FL3731Component::get_setup_priority() const { return setup_priority::DATA; }
+
+void IS31FL3731Component::loop() {
+  display_frame(0);
+}
+
+void IS31FL3731Component::update() {
+  //
+}
+
+display::DisplayType IS31FL3731Component::get_display_type() {
+  return display::DisplayType::DISPLAY_TYPE_GRAYSCALE;
+}
+
+void IS31FL3731Component::fill(Color color) {
+  select_bank(0);
+  auto fill_buffer = std::array<uint8_t, 24>();
+  fill_buffer.fill(color.w);
+
+  for (uint8_t i = 0; i < 6; i++) {
+    write_bytes(0x24 + i * 24, fill_buffer);
+  }
+}
+
+void IS31FL3731Component::draw_pixel_at(int x, int y, Color color) {
+  const uint8_t lednum = x + y * get_width_internal();
+
+  this->set_led_pwm(lednum, color.w, 0);
+}
+
+int IS31FL3731Component::get_height_internal() { return 9; }
+
+int IS31FL3731Component::get_width_internal() {
+  return 16;
+}
+
+void IS31FL3731Component::display_frame(uint8_t bank) {
+  select_bank(ISSI_BANK_FUNCTIONREG); // TODO: check result
+
+  const bool result = write_byte(ISSI_REG_PICTUREFRAME, bank);
+  if (!result) {
+    ESP_LOGE(TAG, "Failed to display frame %d", bank);
+  }
+}
+
+void IS31FL3731Component::select_bank(uint8_t bank) {
+  const bool result = write_byte(ISSI_COMMANDREGISTER, bank);
+  if (!result) {
+    ESP_LOGE(TAG, "Failed to select bank %d", bank);
+  }
+}
+
+void IS31FL3731Component::set_led_pwm(uint8_t lednum, uint8_t pwm, uint8_t bank) {
+  if (lednum >= 144)
+    return;
+
+  this->select_bank(bank); // TODO: check result
+
+  const bool result = this->write_byte(0x24 + lednum, pwm);
+  if (!result) {
+    ESP_LOGE(TAG, "Failed to set LED PWM %d", lednum);
+  }
+}
+
+}
+}
---
+++ esphome/components/is31fl3731/is31fl3731.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace is31fl3731 {
+
+class IS31FL3731Component : public display::Display,
+                            public i2c::I2CDevice {
+ public:
+
+  // ========== INTERNAL METHODS ==========
+  // (In most use cases you won't need these)
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+  void loop() override;
+  void update() override;
+
+  display::DisplayType get_display_type() override;
+  void fill(Color color) override;
+  void draw_pixel_at(int x, int y, Color color) override;
+  int get_height_internal() override;
+  int get_width_internal() override;
+ protected:
+  void display_frame(uint8_t bank);
+  void select_bank(uint8_t bank);
+  void set_led_pwm(uint8_t lednum, uint8_t pwm, uint8_t bank);
+};
+
+}  // namespace is31fl3731
+}  // namespace esphome
